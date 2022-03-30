53af37221e87dc2e06fce7f62bd6f327f39406af
maja-fagman-nti-johanneberg <94617353+maja-fagman-nti-johanneberg@users.noreply.github.com>
2022-03-30 14:01:07 +0200

    små fixat (lagt till kommentarer)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 // Denna fil skall innehålla JavaScript (JS)
 
-const hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger"); 
 const navMenu = document.querySelector(".nav-menu");
+//hämtar taggarna 
 
 hamburger.addEventListener("click", mobileMenu);
+//när jag klickar på menyn händer (nedan) (öppnar)
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
-
-
+//tar bort eller lägger till classerna
 
 const navLink = document.querySelectorAll(".nav-link");
+//hämtar alla taggar (listan-text)
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach(text => text.addEventListener("click", closeMenu));
+//när jag klickar på menyn händer (nedan) (stänger)
 
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+//tar bort classerna
